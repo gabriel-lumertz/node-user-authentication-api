e27f1ce2e2c15c77da05c87bb787e301a040589b
gabriel-lumertz <glumertz10@gmail.com>
2022-04-10 01:32:04 -0300

    adicionada biblioteca http-status-codes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,16 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes'
 
 const usersRoute = Router()
 
 usersRoute.get('/', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'Gabriel'}]
-    res.status(200).send(users)
+    res.status(StatusCodes.OK).send(users)
 })
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid
-    res.status(200).send({ uuid })
+    res.status(StatusCodes.OK).send({ uuid })
 })
 
 export default usersRoute
