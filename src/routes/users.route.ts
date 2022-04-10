d691488d7a978799a2f302463feb3a548ecbe1b3
gabriel-lumertz <glumertz10@gmail.com>
2022-04-10 01:54:36 -0300

    adicionado a rota users.post

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -13,4 +13,9 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, ne
     res.status(StatusCodes.OK).send({ uuid })
 })
 
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body
+    res.status(StatusCodes.CREATED).send(newUser)
+})
+
 export default usersRoute
