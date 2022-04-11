adddde941d94274aab0b951b15d4038be9d1c557
gabriel-lumertz <glumertz10@gmail.com>
2022-04-10 21:50:00 -0300

    criando estrutura para conexão com o banco

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = ''
+const connectionString = 'postgres://niwghuoj:Ffk8ZGxHaJOKz66CFt5WCjJ_UXaKTMS5@kesavan.db.elephantsql.com/niwghuoj'
 
 const db = new Pool({ connectionString })
 
