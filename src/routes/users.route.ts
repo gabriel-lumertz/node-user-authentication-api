3ad23b1b5c1d54032cf2bfef51af4cce599ebaac
gabriel-lumertz <glumertz10@gmail.com>
2022-04-10 22:04:19 -0300

    criando primeira consulta ao bd

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from 'http-status-codes'
+import userRepository from "../repositories/user.repository";
 
 const usersRoute = Router()
 
-usersRoute.get('/', (req: Request, res: Response, next: NextFunction) => {
-    const users = [{ userName: 'Gabriel'}]
+usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userRepository.findAllUsers()
     res.status(StatusCodes.OK).send(users)
 })
 
