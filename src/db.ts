7381ca69c508dc6c918fbb9d88d60619b20bda81
gabriel-lumertz <glumertz10@gmail.com>
2022-04-10 23:58:52 -0300

    ajsute de segurança

--- src/db.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from "pg";
-
-const connectionString = 'postgres://niwghuoj:Ffk8ZGxHaJOKz66CFt5WCjJ_UXaKTMS5@kesavan.db.elephantsql.com/niwghuoj'
-
-const db = new Pool({ connectionString })
-
-export default db
