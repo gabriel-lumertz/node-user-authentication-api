ffad4be1644c891efe543e695f4d4c42aeb44300
gabriel-lumertz <glumertz10@gmail.com>
2022-04-12 00:01:18 -0300

    Desenvolvendo um middleware para reutilizar as funções de autenticação

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,38 +1,18 @@
 import { Request, Response, NextFunction, Router } from "express";
 import ForbiddenError from "../models/errors/forbidden.error.model";
-import userRepository from "../repositories/user.repository";
 import JWT from 'jsonwebtoken'
 import { StatusCodes } from "http-status-codes";
+import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
 
 const authorizationRoute = Router()
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
     
     try {
-        const authorizationHeader = req.headers['authorization']
-    
-        if (!authorizationHeader) {
-            throw new ForbiddenError('Credencias não informadas')
-        }
-
-        const [authenticationType, token] = authorizationHeader.split(' ')
-
-        if (authenticationType !== 'Basic' || !token) {
-            throw new ForbiddenError('Tipo de autenticação inválido')
-        }
-
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
-        
-        const [username, password] = tokenContent.split(':')
-
-        if (!username || !password) {
-            throw new ForbiddenError('Credenciais não preenchidas')
-        }
-
-        const user = await userRepository.findByUsernameAndPassword(username, password)
+        const user = req.user
 
         if (!user) {
-            throw new ForbiddenError('Usuário ou senha inválidos!')
+            throw new ForbiddenError('Usuário não informado!')
         }
 
         const jwtPayload = { username: user.username} 
